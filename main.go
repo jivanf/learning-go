512767ecc8c886b51d3bbe92cce8893a5f8b6f52
Jivan Flores <minecraftjivan@gmail.com>
2018-04-06 18:12:10 -0700

    Slices and for loops

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 )
 
 func main() {
-	card := newCard()
-	log.Println(card)
+	deck := newDeck()
+	for i, card := range deck {
+		log.Println(i, card)
+	}
 }
 
 func newCard() string {
 	return "Ace of Spades"
+}
+
+func newDeck() []string {
+	deck := []string {"Ace of Diamonds", newCard()}
+	deck = append(deck, "Five of Diamonds")
+	return deck
 }
