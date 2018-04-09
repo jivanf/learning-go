d9496c32730284d92d303edf815bb72d0abd4dcf
Jivan <minecraftjivan@gmail.com>
2018-04-09 14:44:22 -0700

    Add newDeck function to deck type

--- deck.go
+++ deck.go
@@ -5,7 +5,20 @@ import "log"
 type deck []string
 
 func (d deck) print() {
-	for i, card := range d {
-		log.Println(i, card)
+	for _, card := range d {
+		log.Println(card)
 	}
+}
+
+func newDeck() deck {
+	d := deck{}
+	cardValues := []string {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardSuits := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			d = append(d, cardValue + " of " + cardSuit)
+		}
+	}
+	return d
 }
--- main.go
+++ main.go
@@ -4,13 +4,3 @@ func main() {
 	d := newDeck()
 	deck.print(d)
 }
-
-func newCard() string {
-	return "Ace of Spades"
-}
-
-func newDeck() deck {
-	deck := deck {"Ace of Diamonds", newCard()}
-	deck = append(deck, "Five of Diamonds")
-	return deck
-}
