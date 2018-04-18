ad887fcdd636697155b58cfb96510cee570553af
Jivan <minecraftjivan@gmail.com>
2018-04-17 18:59:17 -0700

    Embed structs

--- struct/main.go
+++ struct/main.go
@@ -1,1 +1,31 @@
-package _struct
+package main
+
+import "log"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact contactInfo
+}
+
+func main() {
+	person := person{
+		firstName: "John",
+		lastName: "Doe",
+		contact: contactInfo {
+			"johndoe@example.com", 12345,
+			},
+	}
+	log.Println(person)
+
+	person.firstName = "Jane"
+	person.lastName = "Doe"
+	person.contact = contactInfo{"janedoe@example.com", 67890}
+	log.Println(person)
+}
+
