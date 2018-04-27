56a957cc5e42ee02cdf0399095a7074479861eb6
Jivan <minecraftjivan@gmail.com>
2018-04-27 10:56:06 -0700

    Create a logWriter type that implements the Writer interface

--- interface/http/main.go
+++ interface/http/main.go
@@ -7,11 +7,22 @@ import (
 	"io"
 )
 
+type logWriter struct {
+
+}
+
+func (logWriter ) Write(bs []byte) (int, error) {
+	log.Println(string(bs))
+	log.Println("Bytes that were written: ", len(bs))
+	return len(bs), nil
+}
+
 func main() {
 	response, err := http.Get("https://www.google.com")
 	if err != nil {
 		log.Println("Error: ", response)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, response.Body)
+	lw := logWriter{}
+	io.Copy(lw, response.Body)
 }
