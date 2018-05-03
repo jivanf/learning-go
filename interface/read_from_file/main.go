6005fdfa881f753b510e999da0a5eeb4ca716d7c
Jivan <minecraftjivan@gmail.com>
2018-05-02 18:36:30 -0700

    Create program that reads text from a file

--- interface/read_from_file/main.go
+++ interface/read_from_file/main.go
@@ -1,1 +1,17 @@
-package read_from_file
+package main
+
+import (
+	"os"
+	"log"
+	"io"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	bs := make([]byte, 99999)
+	if err == nil {
+		io.Copy(file, file)
+	}
+	log.Println(string(bs))
+
+}
